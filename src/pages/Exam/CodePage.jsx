8b8e26c410ceeee45055634de7d261736ff7c012
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2025-11-08 10:30:24 +0530

    mark issue

--- src/pages/Exam/CodePage.jsx
+++ src/pages/Exam/CodePage.jsx
@@ -1188,7 +1188,7 @@ function CodePage({ question }) {
                               <span className="inline-flex items-center px-2 py-1 rounded-full bg-red-100 text-red-700 font-semibold">
                                 Failed
                               </span>
-                            ) : s.marks*100 === 100  ? (
+                            ) : s.marks === 100  ? (
                               <span className="inline-flex items-center px-2 py-1 rounded-full bg-green-100 text-green-700 font-semibold">
                                 Passed
                               </span>
@@ -1201,7 +1201,7 @@ function CodePage({ question }) {
 
                           {/* Marks */}
                           <td className="px-4 py-2 text-sm text-gray-700 dark:text-gray-300 text-center whitespace-nowrap">
-                            {s.marks * 100}/100
+                            {s.marks }/100
                           </td>
 
 
