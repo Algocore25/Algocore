f149cee61a4b3006322e153651cb02b1734db2b7
HEMANTHKUMARANNAM <hemanthkumar312004@gmail.com>
2026-03-05 22:04:21 +0530

    FIX OUTPUTBUG

--- src/pages/AnimatedTestResults.jsx
+++ src/pages/AnimatedTestResults.jsx
@@ -187,7 +187,7 @@ export default function AnimatedTestResults({ testResults = [], runsubmit }) {
                 <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
-                <span>{currentTest.time ? `${currentTest.time}ms` :  JSON.stringify(currentTest) }</span>
+                <span>{currentTest.time ? `${currentTest.time}ms` :  "N/A" }</span>
               </div>
 
               {/* Memory Used */}
